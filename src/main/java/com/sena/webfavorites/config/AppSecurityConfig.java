f5660f83c4528bd9306ecb0c3060ec1aef94fe18
JuanToledoCol <jpbernal34@misena.edu.co>
2022-09-13 20:28:13 -0500

    litle change

--- src/main/java/com/sena/webfavorites/config/AppSecurityConfig.java
+++ src/main/java/com/sena/webfavorites/config/AppSecurityConfig.java
@@ -23,18 +23,28 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf()
-				.disable().formLogin().disable().httpBasic().disable().exceptionHandling()
-				.authenticationEntryPoint(new RestAuthenticationEntryPoint())
-				.and().authorizeRequests().antMatchers(
-						"/login",
-						"/signup",
-						"/producto",
-						"/productoc/?",
-						"/categoria",
-						"/tiposDocumentos",
-						"/registro"
-						).permitAll().anyRequest().authenticated();
+		http.cors()
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.csrf()
+		.disable()
+		.formLogin()
+		.disable()
+		.httpBasic()
+		.disable()
+		.exceptionHandling()
+		.authenticationEntryPoint(new RestAuthenticationEntryPoint())
+			.and().authorizeRequests().antMatchers(
+					"/login",
+					"/signup",
+					"/producto",
+					"/productoc/?",
+					"/categoria",
+					"/tiposDocumentos",
+					"/registro"
+					).permitAll().anyRequest().authenticated();
 
 		http.addFilterBefore(createTokenAuthFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
